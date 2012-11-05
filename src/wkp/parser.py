86c483bedf76c50dc8c9274cb8b91332ca838139
Johannes Bendler <johannes.bendler@googlemail.com>
2012-11-05 23:56:17 +0100

    pre-branch commit

--- src/wkp/parser.py
+++ src/wkp/parser.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -10,14 +12,15 @@ class ArticleParser:
             
     def _parse(self):
         req = urllib2.Request(self.url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Linux; U; Android 2.2.1; de-DE) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1')
+        req.add_header('User-Agent', 'Mozilla/5.0')
+        #(Linux; U; Android 2.2.1; de-DE) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
         soup = BeautifulSoup(urllib2.urlopen(req).read().encode('utf-8'))
                 
         self.content = soup.find(id = 'mw-content-text').parent.contents
         self.title = soup.find('h1').find('span').string
         self.images = []
         for img in soup.find_all('img'):
-            if int(img.get('height')) > 100 and int(img.get('width')) > 100:
+            if img.get('height') and img.get('width') and int(img.get('height')) > 100 and int(img.get('width')) > 100:
                 self.images.append('http:%s' % (img.get('src')))
         
     def getArticle(self):
