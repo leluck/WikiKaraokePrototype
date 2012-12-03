0c0ffd26d807da3b5c69dcd656780ed168628f9b
Johannes Bendler <johannes.bendler@googlemail.com>
2012-12-03 23:04:06 +0100

    transfer status

--- src/launcher.py
+++ src/launcher.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import wkp.parser
+import wkp.retriever
 import wkp.export
 
 def launch():
     example = 'http://de.wikipedia.org/wiki/Lama_(Kamel)'
     
-    parser = wkp.parser.ArticleParser(example)
-    article = parser.getArticle()
-    export = wkp.export.PDFExporter(article, 'testoutput.pdf')
-    export.write(overwrite = True)
+    receiver = wkp.retriever.ArticleRetriever(example)
+    article = receiver.getArticle()
+    exporter = wkp.export.PDFExporter(article, 'testoutput.pdf')
+    exporter.write(overwrite = True)
     
     #for s in article.sentences:
     #    print('%8.2f - %s' % (s.impact, s.original))
--- src/wkp/retriever.py
+++ src/wkp/retriever.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 import wkp.article
 
-class ArticleParser:
+class ArticleRetriever:
     def __init__(self, url):
         self.url = url
         self._parse()
@@ -17,7 +17,7 @@ class ArticleParser:
         req.add_header('User-Agent', 'Mozilla/5.0')
         #(Linux; U; Android 2.2.1; de-DE) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
         soup = BeautifulSoup(urllib2.urlopen(req).read().encode('utf-8'))
-                
+        
         self.content = soup.find(id = 'mw-content-text').parent.contents
         self.title = soup.find('h1').find('span').string
         self.images = []
