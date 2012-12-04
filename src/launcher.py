62a897f879aef8315085108e7105372f738d2b81
Johannes Bendler <johannes.bendler@googlemail.com>
2012-12-04 01:12:23 +0100

    first running implementation

--- src/launcher.py
+++ src/launcher.py
@@ -10,7 +10,7 @@ import wkp.retriever
 import wkp.export
 
 def launch():
-    example = 'http://de.wikipedia.org/wiki/Stechmücke'
+    example = 'http://de.wikipedia.org/wiki/Lama_(Kamel)'
     
     receiver = wkp.retriever.ArticleRetriever(example)
     article = receiver.getArticle()
--- src/wkp/export.py
+++ src/wkp/export.py
@@ -3,6 +3,7 @@
 import os
 
 import pyfpdf
+import random
 
 class ExporterTargetInvalidException(Exception):
     pass
@@ -14,22 +15,54 @@ class PDFExporter:
     def __init__(self, article, target, overwrite = False):
         self.article = article
         self.target = target
+        self.random = random.Random()
         self.pdf = pyfpdf.FPDF(format = 'A5', orientation = 'L')
         
         self.article.downloadImages(overwrite = False)
         print('Generating PDF contents...')
-        self._createTitlePage()
         
+        self._selectSentences()
+        self._createTitlePage()
+        self._createContentPages()
+    
+    def _selectSentences(self, topSentenceCount = 10, randomSentenceCount = 10):
+        max = len(self.article.sentences)
+        self.sentenceSelection = list()
+        
+        # Add top 10 sentences to selection
+        self.sentenceSelection.extend(self.article.sentences[0 : (max -1 if max < topSentenceCount else topSentenceCount)])
+        # Add random 10 sentences to selection
+        for _ in range(randomSentenceCount):
+            self.sentenceSelection.append(self.random.choice(self.article.sentences[len(self.sentenceSelection):]))
+    
     def _createTitlePage(self):
         self.pdf.add_page()
         
         self.pdf.set_font('Arial', size = 24)
         
         if len(self.article.storedImages) > 0:
-            self.pdf.image(self.article.storedImages[0], x = 10, y = 10, w = 30)
-            self.pdf.cell(50, 50)
+            self.pdf.image(self.article.storedImages[0], x = 10, y = 10, h = 30)
+            self.pdf.cell(45, 30)
         
-        self.pdf.cell(50, 20, txt = self.article.title, align = 'C')
+        self.pdf.multi_cell(135, 10, txt = self.article.title)
+        self.pdf.ln(20)
+        self.pdf.set_font('Arial', size = 16)
+        self.pdf.multi_cell(180, 10, txt = self.random.choice(self.article.sentences).original)
+        
+    def _createContentPages(self):
+        self.pdf.set_margins(25, 15, 25)
+        self.pdf.set_font('Arial', size = 16)
+        
+        sentenceList = self.sentenceSelection[:]
+        
+        while len(sentenceList) > 0:
+            self.pdf.add_page()
+            self.pdf.ln(10)
+            
+            for _ in range(3 if len(sentenceList) > 3 else len(sentenceList)):
+                sentence = sentenceList.pop(self.random.randint(0, len(sentenceList) - 1))
+                self.pdf.multi_cell(160, 6, txt = sentence.original)
+                self.pdf.ln(10)
     
     def write(self, overwrite = False):
         print('Writing PDF file...')
